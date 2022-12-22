38a7ca795e19760405c1f4c50705186d8c001fd7
chandrucrypto <chandra@frontier.xyz>
2022-12-22 16:22:38 +0530

    Implements HOC for Page root

--- src/component/common/page/index.tsx
+++ src/component/common/page/index.tsx
@@ -1,11 +1,12 @@
 import {Stack} from "@mui/material";
 
-export const PageRoot = () => {
+export const PageRoot = ({ children }: any) => {
     return <>
         <Stack sx={{width: "90%", p: "24px"}}
                direction={"column"}
                justifyContent={"flex-start"}>
-        </Stack>;
+            {children}
+        </Stack>
     </>
 
 }
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,6 +4,7 @@ import {SubHeaderText} from "../../component/common/subHeaderText";
 import {About} from "./profile_data";
 import {Margin} from "../../component/common/margin";
 import {Banner} from "../../component/common/banner";
+import {PageRoot} from "../../component/common/page";
 
 type AboutProps = {
     about: About
@@ -12,14 +13,12 @@ type AboutProps = {
 export const AboutPage = ({about}: AboutProps) => {
     return <>
         <Banner image='/assets/images/banner_about.png'></Banner>
-        <Stack sx={{width: "90%", p: "24px"}}
-               direction={"column"}
-               justifyContent={"flex-start"}>
+        <PageRoot>
             <Margin top='24px'></Margin>
             <HeaderText text={about.header}/>
             <Margin top='24px'></Margin>
             <SubHeaderText text={about.subHeader}/>
-        </Stack>;
+        </PageRoot>
     </>
 
 }
