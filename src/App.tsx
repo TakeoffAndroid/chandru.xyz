706718b5b2c217784fd0c1cb9cbecf3819561b2d
chandrucrypto <chandrucrypto@gmail.com>
2023-01-03 10:16:23 +0530

    Splits page content frame as separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,99 @@
+import PageContentFrame from "./pages/pageContentFrame";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import {COLORS} from "./values/colors";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import * as React from "react";
+import NavigationDrawer from "./component/drawer";
 
-import HomePage from "./pages/home";
+const drawerWidth = 240;
 
 export default function App() {
+
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawer = (
+        <div>
+            <NavigationDrawer/>
+        </div>
+    );
+
+    const container = window !== undefined ? () => window.document.body : undefined;
+
     return (
-        <HomePage />
+        <Box sx={{display: 'flex'}}>
+            <CssBaseline/>
+            <AppBar
+                position="fixed"
+                elevation={0}
+                sx={{
+                    backgroundColor: COLORS.white,
+                    width: {sm: `calc(100% - ${drawerWidth}px)`},
+                    ml: {sm: `${drawerWidth}px`},
+                }}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="secondary"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{mr: 2, display: {sm: 'none'}, color: COLORS.black}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Responsive drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Box
+                component="nav"
+                sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
+                aria-label="mailbox folders"
+            >
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: {xs: 'block', sm: 'none'},
+                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        display: {xs: 'none', sm: 'block'},
+                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+                    }}
+                    open
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+            <Box
+                component="main"
+                sx={{flexGrow: 1, p: 3, width: {sm: `calc(100% - ${drawerWidth}px)`}}}
+            >
+                <PageContentFrame/>
+            </Box>
+        </Box>
     )
 }
--- src/pages/pageContentFrame/index.tsx
+++ src/pages/pageContentFrame/index.tsx
@@ -25,17 +25,8 @@ import {EducationPage} from "../education";
 import {AchievementPage} from "../achievements";
 import {ContactPage} from "../contacts";
 
-const drawerWidth = 240;
 
-interface Props {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window?: () => Window;
-}
-
-export default function HomePage(props: Props) {
+export default function PageContentFrame() {
     const json = `{ "about": {
         "header": "About",
         "subHeader": ["Chandrasekar has vast experience in Tech Leadership, Engineering Management, Developer Evangelism, Product Development and Product Management", "Throughout his career, he has been advising and evangelising companies to help with his tech expertise in Blockchain, Mobile and Web apps", "As an Evangelist, he has taken live sessions, contributed to community open sources and spoken at International tech conferences, hackathons, meetups representing Mobile, Web and Blockchain tech."],
@@ -62,84 +53,8 @@ export default function HomePage(props: Props) {
 }
 `
     const profileData: ProfileData = JSON.parse(json);
-    const {window} = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-    };
-
-    const drawer = (
-        <div>
-            <NavigationDrawer/>
-        </div>
-    );
-
-    const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
-        <Box sx={{display: 'flex'}}>
-            <CssBaseline/>
-            <AppBar
-                position="fixed"
-                elevation={0}
-                sx={{
-                    backgroundColor: COLORS.white,
-                    width: {sm: `calc(100% - ${drawerWidth}px)`},
-                    ml: {sm: `${drawerWidth}px`},
-                }}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="secondary"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{mr: 2, display: {sm: 'none'}, color: COLORS.black}}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" noWrap component="div">
-                        Responsive drawer
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Box
-                component="nav"
-                sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
-                aria-label="mailbox folders"
-            >
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                <Drawer
-                    container={container}
-                    variant="temporary"
-                    open={mobileOpen}
-                    onClose={handleDrawerToggle}
-                    ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                    }}
-                    sx={{
-                        display: {xs: 'block', sm: 'none'},
-                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
-                    }}
-                >
-                    {drawer}
-                </Drawer>
-                <Drawer
-                    variant="permanent"
-                    sx={{
-                        display: {xs: 'none', sm: 'block'},
-                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
-                    }}
-                    open
-                >
-                    {drawer}
-                </Drawer>
-            </Box>
-            <Box
-                component="main"
-                sx={{flexGrow: 1, p: 3, width: {sm: `calc(100% - ${drawerWidth}px)`}}}
-            >
                 <Routes>
                     <Route path="/" element={<Navigate to="/about"/>}/>
                     <Route path="/about" element={<AboutPage about={profileData.about}/>}/>
@@ -148,7 +63,5 @@ export default function HomePage(props: Props) {
                     <Route path="/achievement" element={<AchievementPage achievement={profileData.achievement}/>}/>
                     <Route path="/contact" element={<ContactPage contact={profileData.contact}/>}/>
                 </Routes>
-            </Box>
-        </Box>
     );
 }
